b19e156eabcd8cccffb583d4e482619408b9dc47
Sergei <143517331+SeregaSimba@users.noreply.github.com>
2025-06-27 14:07:31 +0300

    feat: img container

--- src/component/main/body.ts
+++ src/component/main/body.ts
@@ -1,5 +1,6 @@
 import "../main/body.scss";
 import profileImg from "../main/profile_Img/profile.jpeg";
+import iconHtml from "../main/profile_Img/src/icons8-html.svg";
 
 export default function MainContainer() {
   const main = document.createElement("main");
@@ -23,5 +24,20 @@ export default function MainContainer() {
   mayImg.classList.add("may-img");
   mayImg.setAttribute("src", profileImg);
   divImgMay.append(mayImg);
+
+  const titleNameH3 = document.createElement("h3");
+  titleNameH3.classList.add("titleNameH3");
+  titleNameH3.textContent = "Lagaev Sergei";
+  containerMayImgSection.append(titleNameH3);
+
+  const containerLanguages = document.createElement("div");
+  containerLanguages.classList.add("container-languages");
+  containerMayImgSection.append(containerLanguages);
+
+  const imgHtml = document.createElement("img");
+  imgHtml.classList.add("img-html");
+  imgHtml.setAttribute("src", "");
+  containerLanguages.append(imgHtml);
+
   // profile image section
 }
